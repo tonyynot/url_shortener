68cc555add0b5ac6bb803a0ceee25b503fa34eba
Anthony Ramella <alramella@gmail.com>
2015-10-12 15:38:11 -0400

    add payload;

--- app/routes/api/generate.php
+++ app/routes/api/generate.php
@@ -1,5 +1,28 @@
 <?php
 
-$app->post('/generate', function() use ($app) {
-  //
+$app->post('/api/generate', function() use ($app) {
+  $payload = json_decode($app->request->getBody());
+
+  if (empty($payload) || empty(trim($payload->url))) {
+    $app->response->setStatus(400);
+
+    return $app->response->write(json_encode([
+      'error' => [
+          'code' => 1000,
+          'message' => 'A URL is required.'
+        ]
+      ]));
+  }
+
+  if (!filter_var($payload->url, FILTER_VALIDATE_URL)) {
+    $app->response->setStatus(400);
+
+    return $app->response->write(json_encode([
+      'error' => [
+          'code' => 1001,
+          'message' => 'A valid URL is required.'
+        ]
+      ]));
+  }
+
 });
